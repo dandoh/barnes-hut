9eda1617b8b775f02514442fefd395cf8bfc082e
dandoh <dainhan605@gmail.com>
2018-01-07 11:40:03 +0700

    data & done sequential

--- main.cpp
+++ main.cpp
@@ -2,25 +2,50 @@
 #include<mpi.h>
 #include<limits.h>
 #include<vector>
+#include<time.h>
 #include<assert.h>
-#include <cmath>
-#define DEBUG(x) std::cout << #x << " = " << x << std::endl;
+#include<cmath>
+
+#define DEBUG(x) std::cerr << #x << " = " << x << std::endl;
 #define DEBUGA(a, b) std::cout << "[ "; for (auto x = (a); x != (b); x++) std::cout << *x << " ";\
   std::cout << "]" << std::endl
 
-
 const double G = 6.67e-11;
 const double Theta = 0.5;
+
+double f_rand(double f_min, double f_max) {
+  double f = (double) rand() / RAND_MAX;
+  return f_min + f * (f_max - f_min);
+}
 
 struct body {
   double mass;
   double x, y;
   double v_x = 0, v_y = 0;
   double f_x = 0, f_y = 0;
+  double r, g, b;
+  friend std::ostream &operator<<(std::ostream &os, const body &body1) {
+    os << "(mass: " << body1.mass << " x: " << body1.x << " y: " << body1.y << " f_x: " << body1.f_x << " f_y: "
+       << body1.f_y << ")";
+    return os;
+  }
 
+  body() {}
   body(double mass, double x, double y) : mass(mass), x(x), y(y) {}
   body(double mass, double x, double y, double v_x, double v_y, double f_x, double f_y)
       : mass(mass), x(x), y(y), v_x(v_x), v_y(v_y), f_x(f_x), f_y(f_y) {}
+
+  void reset_force() {
+    f_x = 0;
+    f_y = 0;
+  }
+
+  void update(double dt) {
+    v_x += dt * f_x / mass;
+    v_y += dt * f_y / mass;
+    x += dt * v_x;
+    y += dt * v_y;
+  }
 };
 
 struct quad {
@@ -64,41 +89,72 @@ class tree_buffer {
  public:
   int n; // number of body
   double length; // length of outter most quad
-  int next;
-  quad *quads; // array of buffer
+  std::vector<quad> quads; // array of buffer
 
   tree_buffer(int n, double length) : n(n), length(length) {
-    quads = new quad[8 * n];
-    quads[0] = quad(0, 0, length);
-    next = 1;
+    quads.push_back(quad(0, 0, length));
   }
-  ~tree_buffer() {
-    delete[] quads;
+
+  void insert(body b) {
+    insert(0, b);
+  }
+
+  void update_force(body &b) {
+    update_force(0, b);
+  }
+
+ private:
+  void put_body(int i, body b) {
+    assert(quads[i].nw != -1 && quads[i].ne != -1 && quads[i].sw != -1 && quads[i].se != -1);
+    assert(quads[i].nw != i && quads[i].ne != i && quads[i].sw != i && quads[i].se != i);
+
+    if (b.x <= quads[i].center_x && b.y >= quads[i].center_y) {
+      insert(quads[i].nw, b);
+    } else if (b.x >= quads[i].center_x && b.y >= quads[i].center_y) {
+      insert(quads[i].ne, b);
+    } else if (b.x <= quads[i].center_x && b.y <= quads[i].center_y) {
+      insert(quads[i].sw, b);
+    } else {
+      insert(quads[i].se, b);
+    }
   }
 
   void insert(int i, body &b) {
-    assert(i < next);
+    if (b.x < -length / 2 || b.x > length / 2 || b.y > length / 2 || b.y < -length / 2) return;
 
     if (quads[i].mass == 0) {
       quads[i].mass = b.mass;
       quads[i].mass_x = b.x, quads[i].mass_y = b.y;
       quads[i].v_x = b.v_x, quads[i].v_y = b.v_y;
+
       quads[i].f_x = b.f_x, quads[i].f_y = b.f_y;
     } else {
       // create 4 children
       if (quads[i].is_external()) {
-        int nw = next++, ne = next++, sw = next++, se = next++;
-        quads[i].nw = nw, quads[i].ne = ne, quads[i].sw = sw, quads[i].se = se;
+        quads.push_back(quad(quads[i].center_x - quads[i].length / 4,
+                         quads[i].center_y + quads[i].length / 4,
+                         quads[i].length / 2));
+        quads[i].nw = (int) (quads.size() - 1);
 
-        quads[nw] = quad(quads[i].center_x - quads[i].length / 4, quads[i].center_y + quads[i].length / 4, length / 2);
-        quads[ne] = quad(quads[i].center_x + quads[i].length / 4, quads[i].center_y + quads[i].length / 4, length / 2);
-        quads[sw] = quad(quads[i].center_x - quads[i].length / 4, quads[i].center_y - quads[i].length / 4, length / 2);
-        quads[nw] = quad(quads[i].center_x + quads[i].length / 4, quads[i].center_y - quads[i].length / 4, length / 2);
+        quads.push_back(quad(quads[i].center_x + quads[i].length / 4,
+                         quads[i].center_y + quads[i].length / 4,
+                         quads[i].length / 2));
+        quads[i].ne = (int) (quads.size() - 1);
 
-        put_body(i, b);
+        quads.push_back(quad(quads[i].center_x - quads[i].length / 4,
+                         quads[i].center_y - quads[i].length / 4,
+                         quads[i].length / 2));
+        quads[i].sw = (int) (quads.size() - 1);
+
+        quads.push_back(quad(quads[i].center_x + quads[i].length / 4,
+                         quads[i].center_y - quads[i].length / 4,
+                         quads[i].length / 2));
+        quads[i].se = (int) (quads.size() - 1);
+
         put_body(i, body(quads[i].mass, quads[i].mass_x, quads[i].mass_y,
                          quads[i].v_x, quads[i].v_y,
                          quads[i].f_x, quads[i].f_y));
+        put_body(i, b);
         quads[i].update_mass_and_pos(b);
       } else {
         quads[i].update_mass_and_pos(b);
@@ -112,11 +168,15 @@ class tree_buffer {
 
     double d = quads[i].distance_to(b);
     if (quads[i].is_external() || (quads[i].length / d) < Theta) {
-      double EPS = 3e4;
       double dx = quads[i].mass_x - b.x;
       double dy = quads[i].mass_y - b.y;
       double dist = sqrt(dx * dx + dy * dy);
-      double F = G * b.mass * quads[i].mass / (dist * dist + EPS * EPS);
+      double EPS = 3e4;
+
+      if (dist == 0) {
+        return;
+      }
+      double F = (G * b.mass * quads[i].mass) / (dist * dist + EPS * EPS);
 
       b.f_x += F * dx / dist;
       b.f_y += F * dy / dist;
@@ -129,34 +189,59 @@ class tree_buffer {
     }
   }
 
-
-  void insert(body b) {
-    insert(0, b);
-  }
-
-  void update_force(body &b) {
-    update_force(0, b);
-  }
-
- private:
-  void put_body(int i, body b) {
-    if (b.x <= quads[i].center_x && b.y >= quads[i].center_y) {
-      insert(quads[i].nw, b);
-    } else if (b.x >= quads[i].center_x && b.y >= quads[i].center_y) {
-      insert(quads[i].ne, b);
-    } else if (b.x <= quads[i].center_x && b.y <= quads[i].center_y) {
-      insert(quads[i].sw, b);
-    } else {
-      insert(quads[i].se, b);
-    }
-  }
-
 };
 
 int main() {
-  tree_buffer tree(10, 16);
-  tree.insert(body(12, 1, 2));
-  tree.insert(body(6, -3, -5));
-  tree.insert(body(6, 1, 1));
-  DEBUG(tree.quads[0].mass_x);
+  freopen("positions.txt", "w", stdout);
+  freopen("inputs/galaxy4.txt", "r", stdin);
+  srand(time(NULL));
+
+  int initial_size;
+  double radius;
+  std::cin >> initial_size >> radius;
+
+  double length = 2 * radius;
+  int current_size = initial_size;
+
+  body bb[initial_size + 5];
+
+  for (int i = 1; i <= initial_size; i++) {
+    double x, y, vx, vy, mass;
+    double r, g, b;
+    std::cin >> x >> y >> vx >> vy >> mass >> r >> g >> b;
+    bb[i] = body(mass, x, y, vx, vy, 0, 0);
+    bb[i].r = r / 255; bb[i].g = g / 255; bb[i].b = b / 255;
+  }
+
+  std::cout << length << "\n";
+  double dt = 0.2;
+  for (double t = 0; t < 100; t += dt) {
+    int sz = 0;
+    for (int i = 1; i <= initial_size; i++) {
+      if (bb[i].x <= length / 2 && bb[i].x >= -length / 2
+          && bb[i].y <= length / 2 && bb[i].y >= -length / 2) {
+        bb[++sz] = bb[i];
+      }
+    }
+    current_size = sz;
+
+
+    for (int i = 1; i <= current_size; i++) {
+      std::cout << bb[i].x << " " << bb[i].y << " "
+                << bb[i].r << " " << bb[i].g << " " << bb[i].b
+                << "\n";
+    }
+    std::cout << "\n";
+
+    tree_buffer tree(current_size, length);
+    for (int i = 1; i <= current_size; i++) {
+      tree.insert(bb[i]);
+    }
+    for (int i = 1; i <= current_size; i++) {
+      bb[i].reset_force();
+      tree.update_force(bb[i]);
+      bb[i].update(dt);
+    }
+  }
+
 }
---
+++ plot.py
@@ -0,0 +1,45 @@
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from itertools import product, combinations
+import sys
+
+
+
+file_object = open(sys.argv[1],"r")
+inp = file_object.readlines()
+# length of simulation box
+length = float(inp[0])
+a = inp[1:]
+
+positions = []
+complete_positions = []
+count = 0
+for i in range(len(a)):
+    if len(a[i].split()) < 5:
+        positions_in_time_step = np.array(positions)
+        complete_positions.append(np.array(positions_in_time_step))
+        count += 1
+        positions = []
+    else:
+        positions.append([float(x) for x in a[i].split()])
+
+
+
+fig = plt.figure()
+ax = fig.gca()
+ax.set_facecolor('black')
+for i in range(0, len(complete_positions)):
+    ax.set_xlim(-length / 2, length / 2)
+    ax.set_ylim(-length / 2, length / 2)
+    plt.pause(0.1)
+    plt.cla()
+    # print(complete_positions[i][:,2:5])
+    p1 = ax.scatter(complete_positions[i][:,0], complete_positions[i][:,1], c=complete_positions[i][:,2:5], marker='o', s=1)
+    # for j in range(0, len(complete_positions[i])):
+    #     [x, y, r, g, b] = complete_positions[i][j]
+    #     ax.plot(x, y, c='w', marker='o', markersize=1)
+
+plt.show()

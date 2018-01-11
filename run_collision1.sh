128fcca0df154335d6eac21d939cecf1034eca98
df <dsf@gmail.com>
2018-01-11 19:54:57 +0700

    run script

---
+++ run_collision1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+ncs=(2 4 8)
+steps=(100 150 200)
+file="collision1.txt"
+input="inputs/${file}"
+
+tuantu_file="${file}_tuantu"
+songsong1may_file="${file}_songsong1may"
+songsong2may_file="${file}_songsong2may"
+
+
+echo "" > ${tuantu_file}
+
+for step in ${steps[*]}
+do
+	echo "-------------step ${step} - ----------------"
+	echo "-------------step ${step} - ----------------" >> ${tuantu_file}
+	mpirun bh -i ${input} -m s -s ${step} >> ${tuantu_file}
+done
+
+echo "" > ${songsong1may_file}
+
+for nc in ${ncs[*]}
+do
+	for step in ${steps[*]}
+	do
+		echo "--------------nc ${nc} step ${step} -------------------"
+		echo "--------------nc ${nc} step ${step} -------------------" >> ${songsong1may_file}
+		mpirun -np ${nc} bh -i ${input} -s ${step} >> ${songsong1may_file}
+	done
+done
+
+echo "" >> ${songsong2may_file}
+for nc in ${ncs[*]}
+do
+	for step in ${steps[*]}
+	do
+		echo "--------------nc ${nc} step ${step} -------------------"
+		echo "--------------nc ${nc} step ${step} -------------------" >> ${songsong2may_file}
+		mpirun -np ${nc} --hostfile hostfile --host localhost,client bh -i ${input} -s ${step} >> ${songsong2may_file}
+	done
+done
